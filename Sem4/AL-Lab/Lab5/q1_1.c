69ca8c25e35a8fd377bf1ddb5db900e50158143c
Anuj Kamath <118060751+AnujKamath@users.noreply.github.com>
2024-02-12 00:10:32 +0530

    Update q1_1.c

--- Sem4/AL-Lab/Lab5/q1_1.c
+++ Sem4/AL-Lab/Lab5/q1_1.c
@@ -1,6 +1,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include "../Headers/stack.h"
 typedef struct n
 {
     int val;
@@ -12,53 +13,6 @@ typedef struct
     int V;
     Node** adjList;
 }Graph;
-typedef struct
-{
-    int *data;
-    int size;
-    int top;
-} Stack;
-
-Stack *createStack(int size)
-{
-    Stack *temp = (Stack *)malloc(sizeof(Stack));
-    temp->size = size;
-    temp->top=-1;
-    temp->data = (int *)calloc(size, sizeof(int));
-    return temp;
-}
-int isEmpty(Stack *s)
-{
-    return (s->top==-1);
-}
-int isFull(Stack *s)
-{
-    return (s->top==s->size-1);
-}
-
-void push(Stack *s, int x)
-{
-    if(isFull(s))
-    printf("Stack is Full");
-    else
-    s->data[++(s->top)]=x;
-}
-
-int pop(Stack *s){
-    if(isEmpty(s)){
-        printf("Queue is empty!\n");
-        return -69;
-    }
-    return s->data[(s->top)--];
-}
-void printStack(Stack* s)
-{
-    printf("Elements in the stack are:");
-    for(int i=0;i<=s->top;i++)
-    {
-        printf("%d",s->data[i]);
-    }
-}
 Node* newNode(int data) {
     Node* node = ( Node*)malloc(sizeof(Node));
     node->val = data;
