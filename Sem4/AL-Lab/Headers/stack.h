6d90d3593944dcbdb79bf19a6a7527f35cc01870
Anuj Kamath <118060751+AnujKamath@users.noreply.github.com>
2024-02-12 00:08:20 +0530

    Update stack.h

--- Sem4/AL-Lab/Headers/stack.h
+++ Sem4/AL-Lab/Headers/stack.h
@@ -1,1 +1,47 @@
+typedef struct
+{
+    int *data;
+    int size;
+    int top;
+} Stack;
 
+Stack *createStack(int size)
+{
+    Stack *temp = (Stack *)malloc(sizeof(Stack));
+    temp->size = size;
+    temp->top=-1;
+    temp->data = (int *)calloc(size, sizeof(int));
+    return temp;
+}
+int isEmpty(Stack *s)
+{
+    return (s->top==-1);
+}
+int isFull(Stack *s)
+{
+    return (s->top==s->size-1);
+}
+
+void push(Stack *s, int x)
+{
+    if(isFull(s))
+    printf("Stack is Full");
+    else
+    s->data[++(s->top)]=x;
+}
+
+int pop(Stack *s){
+    if(isEmpty(s)){
+        printf("Queue is empty!\n");
+        return -69;
+    }
+    return s->data[(s->top)--];
+}
+void printStack(Stack* s)
+{
+    printf("Elements in the stack are:");
+    for(int i=0;i<=s->top;i++)
+    {
+        printf("%d",s->data[i]);
+    }
+}
