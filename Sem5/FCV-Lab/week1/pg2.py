ff953ecc7ce85cfd284347e542c8c2cc4b0f92fb
Anuj Kamath <118060751+AnujKamath@users.noreply.github.com>
2024-07-27 15:44:15 +0530

    Update pg2.py

--- Sem5/FCV-Lab/week1/pg2.py
+++ Sem5/FCV-Lab/week1/pg2.py
@@ -1,19 +1,11 @@
 import cv2
 cap=cv2.VideoCapture('car2.mp4')
-
-def rescale_frame(frame, percent=75):
-    width = int(frame.shape[1] * percent/ 100)
-    height = int(frame.shape[0] * percent/ 100)
-    dim = (width, height)
-    return cv2.resize(frame, dim, interpolation =cv2.INTER_AREA)
-
 if (cap.isOpened()== False):
     print("error")
 while(cap.isOpened()):
     ret, frame = cap.read()
-    frame75 = rescale_frame(frame, percent=20)
-    cv2.imshow('frame75', frame75)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    cv2.imshow('frame', frame)
+    if cv2.waitKey(10) & 0xFF == ord('q'):
         break
 cap.release()
 cv2.destroyAllWindows()
